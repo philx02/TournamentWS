8a849ea2faa7d3389a9a260d85efbb22ba52ea62
Philippe Cayouette <Philippe Cayouette@localhost>
2015-04-11 12:36:28 -0400

    Leaderboard and beautification

--- TournamentWS/Handler/Model.h
+++ TournamentWS/Handler/Model.h
@@ -24,6 +24,7 @@ public:
     {
       throw std::runtime_error(sqlite3_errstr(wResult));
     }
+    mGetTournamentName.reset(new Statement(mSqlite, "SELECT name FROM tournaments WHERE id = ?"));
     mGetPlayers.reset(new Statement(mSqlite, "SELECT id, name FROM players WHERE id IN (SELECT player_id FROM registrations WHERE tournament_id = ?)"));
     mGetResultTable.reset(new Statement(mSqlite, "SELECT player1_id, player2_id, player1_score, player2_score FROM match_results WHERE tournament_id = ?"));
   }
@@ -69,7 +70,19 @@ public:
   {
     std::string wMessage;
 
+    mGetTournamentName->bind(1, iTournamentId);
+    while (mGetTournamentName->runOnce() == SQLITE_ROW)
+    {
+      mGetTournamentName->evaluate([&](sqlite3_stmt *iStatement)
+      {
+        wMessage += std::string(reinterpret_cast< const char * >(sqlite3_column_text(iStatement, 0)));
+      });
+    }
+
+    wMessage += "|";
+
     std::unordered_map< size_t, size_t > wPlayerIdsToIndex;
+    std::vector< PlayerScore > wPlayersScore;
     mGetPlayers->bind(1, iTournamentId);
     while (mGetPlayers->runOnce() == SQLITE_ROW)
     {
@@ -78,7 +91,9 @@ public:
         auto wPlayerId = sqlite3_column_int(iStatement, 0);
         wPlayerIdsToIndex.emplace(wPlayerId, wPlayerIdsToIndex.size());
         wMessage += std::to_string(wPlayerId) + ",";
-        wMessage += std::string(reinterpret_cast< const char * >(sqlite3_column_text(iStatement, 1))) + ",";
+        auto wPlayerName = std::string(reinterpret_cast< const char * >(sqlite3_column_text(iStatement, 1)));
+        wPlayersScore.emplace_back(wPlayerName);
+        wMessage += wPlayerName + ",";
         wMessage += ";";
       });
     }
@@ -95,6 +110,14 @@ public:
         auto wColumn = wPlayerIdsToIndex[sqlite3_column_int(iStatement, 1)];
         auto wScore1 = sqlite3_column_int(iStatement, 2);
         auto wScore2 = sqlite3_column_int(iStatement, 3);
+        if (wScore1 == 2)
+        {
+          wPlayersScore[wRow].mScore += 3;
+        }
+        else if (wScore2 == 2)
+        {
+          wPlayersScore[wColumn].mScore += 3;
+        }
         wScoreTable[wRow][wColumn] = std::to_string(wScore2) + " - " + std::to_string(wScore1);
         wScoreTable[wColumn][wRow] = std::to_string(wScore1) + " - " + std::to_string(wScore2);
       });
@@ -107,6 +130,25 @@ public:
         wMessage += wCell + ",";
       }
       wMessage += ";";
+    }
+
+    wMessage += "|";
+
+    for (auto &&wPlayerScore : wPlayersScore)
+    {
+      wMessage += std::to_string(wPlayerScore.mScore) + ";";
+    }
+
+    wMessage += "|";
+
+    std::sort(std::begin(wPlayersScore), std::end(wPlayersScore), [](decltype(*std::begin(wPlayersScore)) && iLeft, decltype(*std::begin(wPlayersScore)) && iRight)
+    {
+      return iLeft.mScore > iRight.mScore;
+    });
+    for (auto &&wPlayerScore : wPlayersScore)
+    {
+      wMessage += wPlayerScore.mName + ",";
+      wMessage += std::to_string(wPlayerScore.mScore) + ";";
     }
 
     iSender.send(wMessage);
@@ -127,9 +169,17 @@ public:
 
 private:
   sqlite3 *mSqlite;
+  std::unique_ptr< Statement > mGetTournamentName;
   std::unique_ptr< Statement > mGetPlayers;
   std::unique_ptr< Statement > mGetResultTable;
   std::vector< std::weak_ptr< ISender > > mSenders;
   std::mutex mSenderMutex;
   typedef std::lock_guard< decltype(mSenderMutex) > LockGuard;
+
+  struct PlayerScore
+  {
+    PlayerScore(const std::string &iName) : mName(iName), mScore(0) {}
+    std::string mName;
+    size_t mScore;
+  };
 };
