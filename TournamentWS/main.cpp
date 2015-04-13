28156918e9423935616aab563ff3084cbf0ffad2
Philippe Cayouette <Philippe Cayouette@localhost>
2015-04-13 17:26:51 -0400

    fix div by 0

--- TournamentWS/main.cpp
+++ TournamentWS/main.cpp
@@ -7,8 +7,10 @@
 int main(int argc, char *argv[])
 {
   boost::asio::io_service wIoService;
+  auto wDatabase = argc >= 2 ? argv[1] : "db.sqlite";
+  auto wPort = static_cast< uint16_t >(argc >= 3 ? std::strtoul(argv[2], nullptr, 10) : 9002);
   auto wModel = std::make_shared< Model >(argv[1]);
-  auto wTcpServer = createTcpServer(wIoService, Controller(wModel), 9002);
+  auto wTcpServer = createTcpServer(wIoService, Controller(wModel), wPort);
   wIoService.run();
   return 0;
 }
