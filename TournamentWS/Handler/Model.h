a24d8e21c97a088f15a2e45b9374ab7116af3fd0
Philippe Cayouette <Philippe Cayouette@localhost>
2015-04-12 20:59:55 -0400

    matches to go

--- TournamentWS/Handler/Model.h
+++ TournamentWS/Handler/Model.h
@@ -12,6 +12,7 @@
 #include <mutex>
 #include <stdexcept>
 #include <unordered_map>
+#include <iomanip>
 
 class Model : public boost::noncopyable
 {
@@ -68,6 +69,26 @@ public:
 
   void update(ISender &iSender, size_t iTournamentId)
   {
+    doUpdate(iSender, createUpdateMessage(iTournamentId));
+  }
+
+  void updateAll(size_t iTournamentId)
+  {
+    auto wMessage = createUpdateMessage(iTournamentId);
+    LockGuard wLock(mSenderMutex);
+    for (auto &&wSenderWeak : mSenders)
+    {
+      auto wSender = wSenderWeak.lock();
+      if (wSender != nullptr)
+      {
+        doUpdate(*wSender, wMessage);
+      }
+    }
+  }
+
+private:
+  std::string createUpdateMessage(size_t iTournamentId)
+  {
     mGetTournamentName->clear();
     mGetPlayers->clear();
     mGetResultTable->clear();
@@ -103,17 +124,23 @@ public:
     }
 
     wMessage += "|";
-    
+
     std::vector< std::vector< std::string > > wScoreTable(wPlayerIdsToIndex.size(), std::vector< std::string >(wPlayerIdsToIndex.size()));
     mGetResultTable->bind(1, iTournamentId);
+    size_t wMatchesDone = 0;
     while (mGetResultTable->runOnce() == SQLITE_ROW)
     {
+      ++wMatchesDone;
       mGetResultTable->evaluate([&](sqlite3_stmt *iStatement)
       {
         auto wRow = wPlayerIdsToIndex[sqlite3_column_int(iStatement, 0)];
         auto wColumn = wPlayerIdsToIndex[sqlite3_column_int(iStatement, 1)];
         auto wScore1 = sqlite3_column_int(iStatement, 2);
         auto wScore2 = sqlite3_column_int(iStatement, 3);
+        wPlayersScore[wRow].mGameWon += wScore1;
+        wPlayersScore[wRow].mGameLoss += wScore2;
+        wPlayersScore[wColumn].mGameWon += wScore2;
+        wPlayersScore[wColumn].mGameLoss += wScore1;
         if (wScore1 == 2)
         {
           wPlayersScore[wRow].mScore += 3;
@@ -145,33 +172,37 @@ public:
 
     wMessage += "|";
 
+    wMessage += std::to_string((wPlayersScore.size() * wPlayersScore.size() - wPlayersScore.size()) / 2 - wMatchesDone);
+
+    wMessage += "|";
+
     std::sort(std::begin(wPlayersScore), std::end(wPlayersScore), [](decltype(*std::begin(wPlayersScore)) && iLeft, decltype(*std::begin(wPlayersScore)) && iRight)
     {
+      if (iLeft.mScore == iRight.mScore)
+      {
+        auto wTieBreakerLeft = static_cast<double>(iLeft.mGameWon) / static_cast<double>(iLeft.mGameWon + iLeft.mGameLoss);
+        auto wTieBreakerRight = static_cast<double>(iRight.mGameWon) / static_cast<double>(iRight.mGameWon + iRight.mGameLoss);
+        return wTieBreakerLeft > wTieBreakerRight;
+      }
       return iLeft.mScore > iRight.mScore;
     });
     for (auto &&wPlayerScore : wPlayersScore)
     {
       wMessage += wPlayerScore.mName + ",";
-      wMessage += std::to_string(wPlayerScore.mScore) + ";";
+      wMessage += std::to_string(wPlayerScore.mScore) + ",";
+      std::ostringstream wOut;
+      wOut << std::setprecision(3) << (100.0 * static_cast< double >(wPlayerScore.mGameWon) / static_cast< double >(wPlayerScore.mGameWon + wPlayerScore.mGameLoss));
+      wMessage += wOut.str() + "%;";
     }
 
-    iSender.send(wMessage);
+    return wMessage;
   }
 
-  void updateAll(size_t iTournamentId)
+  void doUpdate(ISender &iSender, const std::string &iMessage)
   {
-    LockGuard wLock(mSenderMutex);
-    for (auto &&wSenderWeak : mSenders)
-    {
-      auto wSender = wSenderWeak.lock();
-      if (wSender != nullptr)
-      {
-        update(*wSender, iTournamentId);
-      }
-    }
+    iSender.send(iMessage);
   }
 
-private:
   sqlite3 *mSqlite;
   std::unique_ptr< Statement > mGetTournamentName;
   std::unique_ptr< Statement > mGetPlayers;
@@ -182,8 +213,10 @@ private:
 
   struct PlayerScore
   {
-    PlayerScore(const std::string &iName) : mName(iName), mScore(0) {}
+    PlayerScore(const std::string &iName) : mName(iName), mScore(0), mGameWon(0), mGameLoss(0) {}
     std::string mName;
     size_t mScore;
+    size_t mGameWon;
+    size_t mGameLoss;
   };
 };
