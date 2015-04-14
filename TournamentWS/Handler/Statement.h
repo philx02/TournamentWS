ff1bbc484c5a9d3787e3dd2968f6886e57140a0f
Philippe Cayouette <Philippe Cayouette@localhost>
2015-04-14 16:52:40 -0400

    email works

--- TournamentWS/Handler/Statement.h
+++ TournamentWS/Handler/Statement.h
@@ -61,3 +61,9 @@ void Statement::bind< size_t >(int iLocation, const size_t &iValue)
 {
   sqlite3_bind_int(mStatement, iLocation, iValue);
 }
+
+template<>
+void Statement::bind< std::string >(int iLocation, const std::string &iValue)
+{
+  sqlite3_bind_text(mStatement, iLocation, iValue.c_str(), iValue.size(), SQLITE_TRANSIENT);
+}
