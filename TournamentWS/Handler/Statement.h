2a1da2f9752c20d449e542b040672a690644b060
Philippe Cayouette <Philippe Cayouette@localhost>
2015-07-24 11:37:52 -0400

    now using static_assert instead of throw

--- TournamentWS/Handler/Statement.h
+++ TournamentWS/Handler/Statement.h
@@ -43,7 +43,7 @@ public:
   template< typename T >
   void bind(int iLocation, const T &iValue)
   {
-    throw std::runtime_error("Statement: cannot bind this type");
+    static_assert(false, "Statement: cannot bind this type");
   }
 
   template< typename Lambda >
