a6045d636ecef1b09c68eccdb536a90533f1b2d4
Philippe Cayouette <pcayouette@spoluck.ca>
2015-08-04 15:30:00 -0400

    Added TcpConnection

--- TournamentWS/TcpServer/TcpServer.h
+++ TournamentWS/TcpServer/TcpServer.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include "TcpConnection.h"
 #include <boost/asio.hpp>
 #include <memory>
 #include <boost/bind.hpp>
 
-template< typename PayloadHandler >
+template< template< class > class ConnectionType, class PayloadHandler >
 class TcpServer
 {
 public:
@@ -16,8 +15,8 @@ public:
   }
 
 private:
-  template< typename T >
-  friend TcpServer< T > createTcpServer(boost::asio::io_service &iIoService, const T &iPayloadHandler, unsigned short iPort);
+  template< template< class > class ConnectionType, class PayloadHandler >
+  friend TcpServer< ConnectionType, PayloadHandler > createTcpServer(boost::asio::io_service &, const PayloadHandler &, unsigned short);
 
   TcpServer(boost::asio::io_service &iIoService, const PayloadHandler &iPayloadHandler, unsigned short iPort)
     : mAcceptor(iIoService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), iPort))
@@ -28,15 +27,15 @@ private:
 
   void startAccept()
   {
-    auto wTcpConnection = std::make_shared< TcpConnection< PayloadHandler > >(mAcceptor.get_io_service(), mPayloadHandler);
+    auto wTcpConnection = std::make_shared< ConnectionType< PayloadHandler > >(mAcceptor.get_io_service(), mPayloadHandler);
     mAcceptor.async_accept(wTcpConnection->socket(), boost::bind(&TcpServer::handleAccept, this, wTcpConnection, boost::asio::placeholders::error));
   }
 
-  void handleAccept(std::shared_ptr< TcpConnection< PayloadHandler > > &iTcpConnection, const boost::system::error_code &iError)
+  void handleAccept(std::shared_ptr< ConnectionType< PayloadHandler > > &iConnection, const boost::system::error_code &iError)
   {
     if (!iError)
     {
-      iTcpConnection->listen();
+      iConnection->listen();
       startAccept();
     }
   }
@@ -46,8 +45,8 @@ private:
 };
 
 // Factory Function
-template< typename T >
-TcpServer< T > createTcpServer(boost::asio::io_service &iIoService, const T &iPayloadHandler, unsigned short iPort)
+template< template< class > class ConnectionType, class PayloadHandler >
+TcpServer< ConnectionType, PayloadHandler > createTcpServer(boost::asio::io_service &iIoService, const PayloadHandler &iPayloadHandler, unsigned short iPort)
 {
-  return TcpServer< T >(iIoService, iPayloadHandler, iPort);
+  return TcpServer< ConnectionType, PayloadHandler >(iIoService, iPayloadHandler, iPort);
 }
--- TournamentWS/main.cpp
+++ TournamentWS/main.cpp
@@ -1,4 +1,6 @@
 #include "TcpServer/TcpServer.h"
+#include "TcpServer/WebSocketConnection.h"
+#include "TcpServer/TcpConnection.h"
 #include "Handler/Controller.h"
 #include "Handler/Model.h"
 #include <memory>
@@ -10,7 +12,8 @@ int main(int argc, char *argv[])
   auto wDatabase = argc >= 2 ? argv[1] : "db.sqlite";
   auto wPort = static_cast< uint16_t >(argc >= 3 ? std::strtoul(argv[2], nullptr, 10) : 9002);
   auto wModel = std::make_shared< Model >(argv[1]);
-  auto wTcpServer = createTcpServer(wIoService, Controller(wModel), wPort);
+  auto wWebSocketServer = createTcpServer< WebSocketConnection >(wIoService, Controller(wModel), wPort);
+  auto wTcpServer = createTcpServer< TcpConnection >(wIoService, Controller(wModel), wPort + 1);
   wIoService.run();
   return 0;
 }
